39aefa11350e617e13e2ea76ab04522a10f18079
Amit S Namboothiry <me@amitsn.com>
2019-02-04 15:29:45 -0800

    Initial commit

---
+++ amitsn-blog-api/create.js
@@ -0,0 +1,23 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "AmitsnBlog",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      postId: uuid.v1(),
+      content: data.content,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
