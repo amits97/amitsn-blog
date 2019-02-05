9752cd64658cc763a5abf72340b61245d17accd5
Amit S Namboothiry <me@amitsn.com>
2019-02-04 22:09:04 -0800

    Basic home page

---
+++ amitsn-blog-client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>AmitSN</h1>
+          <p>Random console logs</p>
+        </div>
+      </div>
+    );
+  }
+}
