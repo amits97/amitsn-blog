904b890f1439713228479e28057c1e6583459e2c
Amit S Namboothiry <me@amitsn.com>
2019-02-04 23:32:19 -0800

    Added Nav and basic pages

--- amitsn-blog-client/src/App.js
+++ amitsn-blog-client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import Routes from "./Routes";
 import "./App.css";
 
@@ -8,11 +9,21 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-        <Navbar bg="light">
+        <Navbar bg="light" expand="lg">
           <Navbar.Brand>
             <Link to="/">AmitSN</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <LinkContainer exact to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer exact to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
