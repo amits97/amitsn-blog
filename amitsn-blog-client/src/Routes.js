6533bf2c7012384d9864af6cfdc5b30f4b0b0df4
Amit S Namboothiry <nambooth@amazon.com>
2019-02-05 16:44:19 -0800

    Submit new post now live

--- amitsn-blog-client/src/Routes.js
+++ amitsn-blog-client/src/Routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import NewPost from "./containers/NewPost";
 import NotFound from "./containers/NotFound";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/posts/new" exact component={NewPost} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
---
+++ amitsn-blog-client/src/containers/NewPost.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import { API } from "aws-amplify";
+import "./NewPost.css";
+
+export default class NewPost extends Component {
+  constructor(props) {
+    super(props);
+
+    this.file = null;
+
+    this.state = {
+      isLoading: null,
+      content: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.content.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+  
+    this.setState({ isLoading: true });
+  
+    try {
+      await this.createPost({
+        content: this.state.content
+      });
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  }
+  
+  createPost(post) {
+    return API.post("posts", "/posts", {
+      body: post
+    });
+  }
+
+  render() {
+    return (
+      <div className="NewPost">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="content">
+            <FormControl
+              onChange={this.handleChange}
+              value={this.state.content}
+              as="textarea"
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            bsStyle="primary"
+            disabled={!this.validateForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Create"
+            loadingText="Creating…"
+          />
+        </form>
+      </div>
+    );
+  }
+}
