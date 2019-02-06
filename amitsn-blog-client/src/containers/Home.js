aa2d9822515a9e76349c7e3d1db32a1e2409094f
Amit S Namboothiry <nambooth@amazon.com>
2019-02-06 15:41:01 -0800

    Blog post listing

--- amitsn-blog-client/src/containers/Home.js
+++ amitsn-blog-client/src/containers/Home.js
@@ -1,7 +1,69 @@
 import React, { Component } from "react";
+import { Container, Card } from "react-bootstrap";
+import TimeAgo from "javascript-time-ago"
+import en from "javascript-time-ago/locale/en"
+import english from "javascript-time-ago/locale/en"
+import { API } from "aws-amplify";
 import "./Home.css";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    TimeAgo.addLocale(en);
+    this.timeAgo = new TimeAgo('en-US');
+
+    this.state = {
+      isLoading: true,
+      posts: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const posts = await this.posts();
+      this.setState({ posts });
+    } catch (e) {
+      alert(e);
+    }
+  
+    this.setState({ isLoading: false });
+  }
+
+  posts() {
+    return API.get("posts", "/posts");
+  }
+
+  renderPostsList(posts) {
+    return [].concat(posts).map(
+      (post, i) =>
+        <Container>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <b>{"Post " + (i+1)}</b>
+              </Card.Title>
+              <Card.Text>
+                {post.content}
+              </Card.Text>
+              <small className="text-muted">
+                {this.timeAgo.format(new Date(post.createdAt), english.long)}
+              </small>
+            </Card.Body>
+          </Card>
+          <br />
+        </Container>
+    );
+  }
+
+  renderPosts() {
+    return (
+      <div className="posts">
+        {!this.state.isLoading && this.renderPostsList(this.state.posts)}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
@@ -9,6 +71,7 @@ export default class Home extends Component {
           <h1>AmitSN</h1>
           <p>Random console logs</p>
         </div>
+        { this.renderPosts() }
       </div>
     );
   }
--- amitsn-blog-client/src/containers/NewPost.js
+++ amitsn-blog-client/src/containers/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { FormGroup, FormControl } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { API } from "aws-amplify";
 import "./NewPost.css";
