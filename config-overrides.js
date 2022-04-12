df86b273464661c5b09e41869fdfb6adb56a7d2d
Amit S Namboothiry <me@amitsn.com>
2022-04-12 20:16:59 +0530

    Remove contenthash from webpack output files

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,16 @@
 module.exports = function override (config, env) {
-  let loaders = config.resolve
-  loaders.fallback = {
-    "path": require.resolve("path-browserify")
+  if (env === "production") {
+    //JS Overrides
+    config.output.filename = 'static/js/[name].js';
+
+    //CSS Overrides
+    config.plugins[5].options.filename = 'static/css/[name].css';
   }
+
+  // Resolve path
+  config.resolve.fallback = {
+    "path": require.resolve("path-browserify")
+  };
+
   return config;
 }
