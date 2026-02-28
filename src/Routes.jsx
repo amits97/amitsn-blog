a199e3108bc7fa059755ed2e087e8ea7d7833524
Amit S Namboothiry <me@amitsn.com>
2026-02-28 18:07:27 +0530

    Lazy load modules

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,27 +1,77 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
-import Login from "./containers/Login";
-import NewPost from "./containers/NewPost";
-import Posts from "./containers/Posts";
-import Admin from "./containers/Admin";
-import NotFound from "./containers/NotFound";
 
-const Routes = ({ childProps }) =>
-  <Switch>
-    <AppliedRoute path="/" exact component={Posts} props={{isPage: true, ...childProps}} />
-    <AuthenticatedRoute path="/admin" exact component={Admin} props={childProps} />
-    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
-    <AuthenticatedRoute path="/admin/new-post" exact component={NewPost} props={childProps} />
-    <AuthenticatedRoute path="/admin/edit-post/:id" exact component={NewPost} props={{isEditMode: true, ...childProps}} />
-    <AppliedRoute path="/blog" exact component={Posts} props={childProps} />
-    <AppliedRoute path="/blog/all" exact component={Posts} props={{allPosts: true, ...childProps}} />
-    <AppliedRoute path="/blog/:id" exact component={Posts} props={childProps} />
-    <AppliedRoute path="/:id" exact component={Posts} props={{isPage: true, ...childProps}} />
-    { /* Finally, catch all unmatched routes */ }
-    <Route component={NotFound} />
-  </Switch>;
+const Login = lazy(() => import("./containers/Login"));
+const NewPost = lazy(() => import("./containers/NewPost"));
+const Posts = lazy(() => import("./containers/Posts"));
+const Admin = lazy(() => import("./containers/Admin"));
+const NotFound = lazy(() => import("./containers/NotFound"));
+
+const Routes = ({ childProps }) => (
+  <Suspense
+    fallback={
+      <div className="loader">
+        <div className="loading-logo">a.</div>
+      </div>
+    }
+  >
+    <Switch>
+      <AppliedRoute
+        path="/"
+        exact
+        component={Posts}
+        props={{ isPage: true, ...childProps }}
+      />
+      <AuthenticatedRoute
+        path="/admin"
+        exact
+        component={Admin}
+        props={childProps}
+      />
+      <UnauthenticatedRoute
+        path="/login"
+        exact
+        component={Login}
+        props={childProps}
+      />
+      <AuthenticatedRoute
+        path="/admin/new-post"
+        exact
+        component={NewPost}
+        props={childProps}
+      />
+      <AuthenticatedRoute
+        path="/admin/edit-post/:id"
+        exact
+        component={NewPost}
+        props={{ isEditMode: true, ...childProps }}
+      />
+      <AppliedRoute path="/blog" exact component={Posts} props={childProps} />
+      <AppliedRoute
+        path="/blog/all"
+        exact
+        component={Posts}
+        props={{ allPosts: true, ...childProps }}
+      />
+      <AppliedRoute
+        path="/blog/:id"
+        exact
+        component={Posts}
+        props={childProps}
+      />
+      <AppliedRoute
+        path="/:id"
+        exact
+        component={Posts}
+        props={{ isPage: true, ...childProps }}
+      />
+      {/* Finally, catch all unmatched routes */}
+      <Route component={NotFound} />
+    </Switch>
+  </Suspense>
+);
 
 export default Routes;
